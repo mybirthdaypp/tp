5b8641c793705328d30b1bbff2d750afeb1ae0a5
mybirthdaypp <84100218+mybirthdaypp@users.noreply.github.com>
2021-05-13 01:05:38 +0530

    updae

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Pratiksha" && b=="29-052000")
+  	if(a== "Pratiksha" && b=="29-05-2000")
   	{
   	    window.location.href="start.html";
   	}
